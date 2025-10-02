f28aa12905ca03ab8f355fba290f241bd6fafc85
Kelly McBride <kemcbride28@gmail.com>
2025-10-02 09:22:45 -0700

    Created blank demo route (still testing CI)

--- flask/website.py
+++ flask/website.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("home.html") 
+
+
+@app.route("/demo")
+def datavis_demo():
+    return render_template("datavis_demo.html")
