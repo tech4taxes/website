817ad0638c1b40663bae50a09b3590c002be3f22
Fatema Boxwala <fatemabox@gmail.com>
2025-10-02 11:47:29 -0700

    Create b_o_data.py

---
+++ b_o_data.py
@@ -0,0 +1,44 @@
+import requests
+import pprint
+from bs4 import BeautifulSoup
+
+
+# _get_data pulls given google doc link from the web and parses the HTML content into 
+# a nice dict for use
+def _get_data(link):
+    # Use the BeautifulSoup library to parse the HTML and pull out the table containing coords
+    document = requests.get(link)
+    html_content = BeautifulSoup(document.text, features="html.parser")
+    tables = html_content.find_all("table")
+    data = []
+    
+    # Iterate through the table and create a dictionary of the form dict[(x,y)] = character
+    for table in tables:
+        rows = table.find_all("tr")
+        name = rows[0].text
+        for row in table.find_all("tr"):
+            cols = row.find_all("td")
+            if "Naics" in cols[0].text:
+                continue
+            if "Total" in cols[0].text:
+                continue
+            cols = [c.text for c in cols]
+            for c in cols:
+                if c[0] == "$":
+                    cols[cols.index(c)] = int(c[1:].replace(",", ""))
+
+            if isinstance(cols[1], str):
+                continue
+
+            data.append(tuple(cols))
+
+    data.sort(key=lambda x: float(x[3]) / float(x[1]))
+    b = {}
+    for a in data:
+        b[a[0].replace('\xa0', '')] = (a[1], 100*(float(a[3]) / float(a[1])))
+ 
+    for e in b.keys(): 
+         print(f"{e}\n    Gross receipts: {b[e][0]}\n    % Paid B&O: {b[e][1]}")
+
+
+_get_data("https://apps.dor.wa.gov/ResearchStats/Content/QuarterlyBusinessReview/Results5.aspx?Period=2025Q1&Type=naics&Format=HTML")
