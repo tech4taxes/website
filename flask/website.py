1e03450d71d5dbb05ae67029a56a2ef423b16455
Kelly McBride <kellym@nvidia.com>
2025-10-05 09:02:31 -0700

    pylint-CI: add .pylintrc, fix lints, add deps for bodata

--- flask/website.py
+++ flask/website.py
@@ -1,12 +1,12 @@
+"""flask/website.py - Tech4Taxes website flask route/app file."""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def hello_world():
-    return render_template("home.html") 
+    return render_template("home.html")
 
 
 @app.route("/demo")
