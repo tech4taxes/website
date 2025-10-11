b5a3e2c4d07bc2a37d2180751506c1dd9de08733
Kelly McBride <kellym@nvidia.com>
2025-10-10 17:19:08 -0700

    Change parent gitignore, add tsconfig and vite config

---
+++ flask/vite.config.js
@@ -0,0 +1,25 @@
+import {defineConfig} from "vite";
+import {globSync} from "glob";
+import path from "node:path";
+
+export default defineConfig({
+    server: {
+        cors: {
+            origin: ["http://127.0.0.1:8000", "http://localhost:8000"],
+        },
+    },
+    build: {
+        manifest: true,
+        rollupOptions: {
+            input: Object.fromEntries(globSync("./vite-src/**/index.ts").map(file => [
+                path.dirname(path.relative("vite-src", file)),
+                file 
+            ])),
+            preserveEntrySignatures: "strict",
+            commonjsOptions: { transformMixedEsModules: true },
+        },
+        outDir: "",
+        emptyOutDir: false,
+        assetsDir: "static/dist",
+    },
+});
