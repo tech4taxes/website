8638fcd324cf9416725c106a989b8568827122b6
Kelly McBride <kellym@nvidia.com>
2025-10-15 23:44:51 -0700

    Add scatterplot to demo/bno

--- flask/vite-src/demo_bno/index.ts
+++ flask/vite-src/demo_bno/index.ts
@@ -16,8 +16,40 @@ const D3Colors = [
   '#e377c2',
   '#7f7f7f',
   '#bcbd22',
+  '#17becf',
+  '#1f77b4',
+  '#ff7f0e',
+  '#2ca02c',
+  '#d62728',
+  '#9467bd',
+  '#8c564b',
+  '#e377c2',
+  '#7f7f7f',
+  '#bcbd22',
+  '#17becf',
+  '#1f77b4',
+  '#ff7f0e',
+  '#2ca02c',
+  '#d62728',
+  '#9467bd',
+  '#8c564b',
+  '#e377c2',
+  '#7f7f7f',
+  '#bcbd22',
+  '#17becf',
+  '#1f77b4',
+  '#ff7f0e',
+  '#2ca02c',
+  '#d62728',
+  '#9467bd',
+  '#8c564b',
+  '#e377c2',
+  '#7f7f7f',
+  '#bcbd22',
   '#17becf'
 ];
+
+const really_big_color_list = Array(10).fill(D3Colors).flat()
 
 // Load bno data:
 dfd.readCSV("../../static/data/b_o_data.csv") //assumes file is in CWD
@@ -32,25 +64,33 @@ dfd.readCSV("../../static/data/b_o_data.csv") //assumes file is in CWD
         x: head.Name.values.concat(tail.Name.values),
         y: head.PERCENTAGE_PAID.values.concat(tail.PERCENTAGE_PAID.values),
         type: 'bar',
-        marker: { color:D3Colors },
+        marker: { color: really_big_color_list },
       },
     ];
 
     const container1 = document.getElementById("container1");
     Plotly.newPlot(container1, woohoo, {
-      title: {text: "B&O Numbers First 5"},
+      title: {text: "B&O Numbers Top 5 & Bottom 5"},
     });
 
+
+    // Scatterplot of revenue to pct paid
+    const container2 = document.getElementById("container2");
+    Plotly.newPlot(container2, [{
+      x: df.TOTAL_REVENUE.values,
+      y: df.PERCENTAGE_PAID.values,
+      text: df.Name.values,
+      marker: { color: really_big_color_list},
+      type: 'scatter',
+      mode: 'markers',
+    }],
+      {
+      title: {text: "Scatterplot Percentage paid vs. Revenue"},
+      // margin: { t: 0 },
+      automargin: true,
+    });
   }).catch(err=>{
     console.log(err);
 })
 
 
-const container2 = document.getElementById("container2");
-Plotly.newPlot(container2, [{
-  x: [1, 2, 3, 4, 5],
-  y: [1, 2, 4, 8, 16] }],
-  {
-  title: {text: "Squared Numbers"},
-  margin: { t: 0 },
-});
